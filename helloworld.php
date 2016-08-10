c9d85c3f360614105475e78ce7e6e909453b7c50
manikandanshan <manikbhuvan1985@gmail.com>
2016-08-10 10:27:22 +0530

    committed

--- helloworld.php
+++ helloworld.php
@@ -15,9 +15,9 @@ lines
 
 // You can also use comments to leave out parts of a code line
 $x = 5 /* + 15 */ + 5;
-echo $x;
-echo "helloworld";
-echo "helloworld1";
+echo $x\n;
+echo "helloworld\n";
+echo "helloworld1\n";
 echo"raj1";
 ?>
 
